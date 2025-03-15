5d95b20bd97ab967a7693418bae296da20039276
Gustavo Cunha <gustavocunhaads@gmail.com>
2025-03-15 17:28:47 -0300

    Adicionando coluna titulo na rota /noticias

--- index.js
+++ index.js
@@ -166,7 +166,7 @@ app.get('/noticias', async (req, res) => {
 
     const result = await pool.query(
       `
-        SELECT data, portal
+        SELECT data, portal, titulo
         FROM noticias
         WHERE TO_DATE(data, 'DD/MM/YYYY') BETWEEN TO_DATE($1, 'YYYY-MM-DD') AND TO_DATE($2, 'YYYY-MM-DD')
         ORDER BY TO_DATE(data, 'DD/MM/YYYY') DESC
@@ -179,7 +179,8 @@ app.get('/noticias', async (req, res) => {
 
     const data = result.rows.map(row => ({
       data: row.data || '',
-      portal: row.portal || 'Desconhecido'
+      portal: row.portal || 'Desconhecido',
+      titulo: row.titulo || 'Título Não Disponível'
     }));
 
     res.json(data);
