0a8e88b19f3be0dc9e0585370bfe1bb110e35d0c
Gustavo Cunha <gustavocunhaads@gmail.com>
2025-03-14 22:34:26 -0300

    Adicionando filtro de data na API

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 // Adicionar suporte a CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Permite qualquer origem (para testes)
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
@@ -18,7 +18,19 @@ const pool = new Pool({
 // Rota para pegar as métricas
 app.get('/metrics', async (req, res) => {
   try {
-    const result = await pool.query('SELECT COUNT(*) as total_mencoes FROM noticias');
+    // Obter parâmetros de data da query string
+    const { from, to } = req.query;
+
+    let query = 'SELECT COUNT(*) as total_mencoes FROM noticias';
+    let queryParams = [];
+
+    // Se from e to forem fornecidos, adicionar filtro de data
+    if (from && to) {
+      query += ' WHERE created_at BETWEEN $1 AND $2';
+      queryParams = [from, to];
+    }
+
+    const result = await pool.query(query, queryParams);
     const totalMencoes = parseInt(result.rows[0].total_mencoes) || 0;
     res.json({ total_mencoes: totalMencoes });
   } catch (error) {
