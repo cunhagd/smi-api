c697800c85d1727f29af7c0d84090884df9de2f1
Isabela Bento <isabelaebento@gmail.com>
2025-04-01 13:49:58 -0300

    fix: ajusta CORS para permitir método PUT

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
  const { Pool } = require('pg');
+ const cors = require('cors'); 
  const app = express();
  const port = process.env.PORT || 3000;
  
  app.use(express.json()); // Para processar requisições com corpo JSON
- app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', '*');
-   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-   next();
- });
+
+ // Configuração de CORS
+ app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept'],
+}));
  
  const pool = new Pool({
    connectionString: process.env.DATABASE_URL
